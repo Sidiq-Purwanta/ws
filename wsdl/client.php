a01e2a9c99b2b107dc0816caf49904b135e1c252
sidiq-purwanta <sidiq.purwanta@mail.ugm.ac.id>
2014-10-08 08:25:01 +0700

    client.php

---
+++ wsdl/client.php
@@ -0,0 +1,21 @@
+<?php 
+	//memulai
+	session
+	session_start(
+	);
+	//panggil library
+	require_once('nusoap/lib/nusoap.php');
+	//mendefinisikan alamat url serveice yang disediakan oleh client
+	$client = new
+	soapclient('http://localhost/login/sever.php?wsdl',true);
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$result = $client->call('login_ws',
+			array('username'=>$username,'password'=>$password));
+	if($result == "Login Berhasil"){
+		$_SESSION['username']=$username;
+		header ("location:index.php");
+	} else{
+		header ("location:login.php");
+	}
+?>
