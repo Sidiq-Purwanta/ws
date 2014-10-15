8e129b8cd6f635d799d579815ea00399480dfade
sidiq-purwanta <sidiqpurwanta@mail.ugm.ac.id>
2014-10-15 07:35:00 +0700

    add client.php

--- wsdl/client.php
+++ wsdl/client.php
@@ -1,11 +1,10 @@
 <?php 
 	//memulai session
-	session_start(
-	);
+	session_start();
 	//panggil library
 	require_once('nusoap/lib/nusoap.php');
 	//mendefinisikan alamat url serveice yang disediakan oleh client
-	$client = newsoapclient('http://localhost/login/server.php?wsdl',true);
+	$client = new nusoap_client('http://localhost/login/server.php?wsdl',true);
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	$result = $client->call('login_ws',
